70526d413cfe6e2790181ce2d9d1215ac29f5650
marmarti <markwinap@gmail.com>
2022-04-22 16:01:07 -0500

    Added Error Request Reponse

--- FileHandlerHttpTrigger.cs
+++ FileHandlerHttpTrigger.cs
@@ -27,53 +27,62 @@ namespace POCAzureFunctions
             List<string> rowList = new List<string>();
             ISheet sheet;
 
-            var formdata = await req.ReadFormAsync();
-            var file = req.Form.Files["file"];
-            log.LogInformation("File - {0} - {0} - {0}", file.FileName, file.Length, file.ContentType);
-
-            // Read the file content   
-            var stream = file.OpenReadStream();
-
-            XSSFWorkbook xssWorkbook = new XSSFWorkbook(stream);
-            sheet = xssWorkbook.GetSheetAt(0);
-            IRow headerRow = sheet.GetRow(0);
-            int cellCount = headerRow.LastCellNum;
-
-            log.LogInformation("Cells {0}", cellCount);
-
-            //Add Table Headers
-            for (int j = 0; j < cellCount; j++)
+            try
             {
-                ICell cell = headerRow.GetCell(j);
-                if (cell == null || string.IsNullOrWhiteSpace(cell.ToString())) continue;
+                var formdata = await req.ReadFormAsync();
+                var file = req.Form.Files["file"];
+                log.LogInformation("File - {0} - {0} - {0}", file.FileName, file.Length, file.ContentType);
+
+                // Read the file content   
+                var stream = file.OpenReadStream();
+
+                XSSFWorkbook xssWorkbook = new XSSFWorkbook(stream);
+                sheet = xssWorkbook.GetSheetAt(0);
+                IRow headerRow = sheet.GetRow(0);
+                int cellCount = headerRow.LastCellNum;
+
+                log.LogInformation("Cells {0}", cellCount);
+
+                //Add Table Headers
+                for (int j = 0; j < cellCount; j++)
                 {
-                    dtTable.Columns.Add(cell.ToString());
-                } 
-            }
-            // Add Table Rows
-            for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++)
-            {
-                IRow row = sheet.GetRow(i);
-                // If row is null skip it
-                if (row == null) continue;
-                // If row cells are empty skip it
-                if(row.Cells.TrueForAll(c => c.CellType == CellType.Blank)) continue;
-                for (int j = row.FirstCellNum; j < cellCount; j++)
-                {
-                    if (row.GetCell(j) != null)
+                    ICell cell = headerRow.GetCell(j);
+                    if (cell == null || string.IsNullOrWhiteSpace(cell.ToString())) continue;
                     {
-                        if (!string.IsNullOrEmpty(row.GetCell(j).ToString()) && !string.IsNullOrWhiteSpace(row.GetCell(j).ToString()))
-                        {
-                            rowList.Add(row.GetCell(j).ToString());
-                        }
+                        dtTable.Columns.Add(cell.ToString());
                     }
                 }
-                if(rowList.Count>0)
-                dtTable.Rows.Add(rowList.ToArray());
-                rowList.Clear(); 
+                // Add Table Rows
+                for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++)
+                {
+                    IRow row = sheet.GetRow(i);
+                    // If row is null skip it
+                    if (row == null) continue;
+                    // If row cells are empty skip it
+                    if (row.Cells.TrueForAll(c => c.CellType == CellType.Blank)) continue;
+                    for (int j = row.FirstCellNum; j < cellCount; j++)
+                    {
+                        if (row.GetCell(j) != null)
+                        {
+                            if (!string.IsNullOrEmpty(row.GetCell(j).ToString()) && !string.IsNullOrWhiteSpace(row.GetCell(j).ToString()))
+                            {
+                                rowList.Add(row.GetCell(j).ToString());
+                            }
+                        }
+                    }
+                    if (rowList.Count > 0)
+                        dtTable.Rows.Add(rowList.ToArray());
+                    rowList.Clear();
+                }
+
+                return new OkObjectResult(JsonConvert.SerializeObject(dtTable));
+            }
+            catch (Exception ex)
+            {
+                log.LogInformation("Exception - {0}", ex.Message);
+                return new BadRequestObjectResult(ex.Message);
             }
 
-            return new OkObjectResult(JsonConvert.SerializeObject(dtTable));
         }
 
     }
